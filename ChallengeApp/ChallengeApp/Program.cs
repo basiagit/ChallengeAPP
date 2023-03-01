b3622f01085296671aeaf88e874f6dd6061ef618
BarbaraSzczotka <barbara.szczotka2007@gmail.com>
2023-03-01 17:05:23 +0100

    Lekcja 4 . Kobieta , Ewa lat 33.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "female";
+var age = 33;
+if (gender == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age ==33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}   
+else if (gender != "female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("osoba nie spełniająca warunków");
+}
+
